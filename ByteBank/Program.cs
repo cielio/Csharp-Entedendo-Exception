50d1798f0ada76235f1c83fc5aa3f11582151b50
cielio <cielion@hotmail.com>
2021-01-17 15:27:30 -0300

    Testes com abrir arquivos

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,27 +11,40 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            try
-            {
-                ContaCorrente conta1 = new ContaCorrente(4564, 789684);
-                ContaCorrente conta2 = new ContaCorrente(7891, 456794);
-
-                //conta1.Transferir(10000, conta2);
-                conta1.Sacar(10000);
-            }
-            catch (OperacaoFinanceiraException e)
-            {
-                Console.WriteLine(e.Message);
-                Console.WriteLine(e.StackTrace);
-
-                Console.WriteLine("Informações da INNER EXCEPTION (exceção interna):");
-
-                //Console.WriteLine(e.InnerException.Message);
-                //Console.WriteLine(e.InnerException.StackTrace);
-            }
+            CarregarContas();
 
             Console.WriteLine("Execução finalizada. Tecle enter para sair");
             Console.ReadLine();
+        }
+
+        private static void CarregarContas()
+        {
+            using (LeitorDeArquivo leitor = new LeitorDeArquivo("contas.txt"))
+            {
+                leitor.LerProximaLinha();
+                leitor.LerProximaLinha();
+                leitor.LerProximaLinha();
+            }
+
+            //try
+            //{
+            //    leitor = new LeitorDeArquivo("contas.txt");
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //}
+            //catch (IOException)
+            //{
+            //    Console.WriteLine("Exceção do tipo IOException capturada e tratada.");
+            //}
+            //finally
+            //{
+            //    if (leitor != null)
+            //    {
+            //        leitor.Fechar();
+            //    }
+            //}
+
         }
 
         // Teste com a cadeia de chamada:
