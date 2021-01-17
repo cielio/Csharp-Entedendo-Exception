b24dbfaa164e55fa3db731d360d86f12067ef2ce
cielio <cielion@hotmail.com>
2021-01-17 09:06:01 -0300

    first commit#2

---
+++ ByteBank/ContaCorrente.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+
+namespace ByteBank
+{
+    public class ContaCorrente
+    {
+        private static int TaxaOperacao;
+
+        public static int TotalDeContasCriadas { get; private set; }
+
+        public Cliente Titular { get; set; }
+
+        public int Numero { get; }
+        public int Agencia { get; }
+
+        private double _saldo = 100;
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            if (numero <= 0)
+            {
+                throw new ArgumentException("O argumento agencia deve ser maior que 0.", nameof(agencia));
+            }
+
+            if(numero <= 0)
+            {
+                throw new ArgumentException("O argumento numero deve ser maior que 0.", nameof(numero));
+            }
+
+            Agencia = agencia;
+            Numero = numero;
+
+            TotalDeContasCriadas++;
+            TaxaOperacao = 30 / TotalDeContasCriadas;
+        }
+
+        public void Sacar(double valor)
+        {
+            if (valor < 0)
+            {
+                throw new ArgumentException("Valor de saue nao pode ser negativo", nameof(valor));
+            }
+            if (_saldo < valor)
+            {
+                throw new SaldoInsuficienteException(_saldo, valor);
+            }
+
+            _saldo -= valor;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public void Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (valor < 0)
+            {
+                throw new ArgumentException("Valor de saue nao pode ser negativo", nameof(valor));
+            }
+
+            Sacar(valor);
+            contaDestino.Depositar(valor);
+        }
+    }
+}
